569e23fc6158c7770334793e8181b825d937247b
agz1990 <522360568@qq.com>
2013-09-07 18:45:27 +0800

    同步数据

--- project/autoBuildLanguage/Rules.py
+++ project/autoBuildLanguage/Rules.py
@@ -3,6 +3,49 @@ Created on 2013年9月6日
 
 @author: hp41
 '''
+# 英语、法语、西班牙语、葡萄牙语、德语、意大利语        西欧Windows
+# 俄语                                                                                       西里尔文Windows
+# 阿拉伯语                                                                                 ASMO708
+# 中文                                                                                        简体中文（GB2312）    
+# 繁体中文                                                                                 繁体中文（Big5）    
+# 泰文                                                                                        泰文Windows
+# 越南语                                                                                    越南文Windows
+# 印尼语                                                                                    西欧Windows
+# 土耳其语                                                                                土耳其文Windows
+# 波斯语                                                                                    阿拉伯文Windows
+
+LANGUAGE_S = 'gb2312'
+LANGUAGE_E = 'cp1252'
+LANGUAGE_T = 'big5'
+LANGUAGE_P = 'cp1252'
+LANGUAGE_a = 'cp1252'
+LANGUAGE_I = 'cp1252'
+LANGUAGE_R = 'cp1251'
+LANGUAGE_F = 'cp1252'
+LANGUAGE_B = 'iso8859_6'
+LANGUAGE_G = 'cp1252'
+LANGUAGE_t = 'cp1254'
+
+#工程路径配置:
+DESK_DIR = '' # 当前系统桌面路径
+DEST_DIR = 'obj' # 生成目标路径
+SUB_DIR  = 'lang' # 相对于 ‘DEST_DIR’ 小写语言的目标路径 
+
+
+
+CodeingMap = {
+            'S':LANGUAGE_S,
+            'E':LANGUAGE_E,
+            'T':LANGUAGE_T,
+            'P':LANGUAGE_P,
+            'a':LANGUAGE_a,
+            'I':LANGUAGE_I,
+            'R':LANGUAGE_R,
+            'F':LANGUAGE_F,
+            'B':LANGUAGE_B,
+            'G':LANGUAGE_G,
+            't':LANGUAGE_t,
+            }
 
 
 if __name__ == '__main__':
--- project/autoBuildLanguage/autoBuild.py
+++ project/autoBuildLanguage/autoBuild.py
@@ -3,127 +3,102 @@ Created on 2013年9月6日
 
 @author: hp41
 '''
+
+from autoBuildLanguage import Rules
 import excelPack
 import os
 import string
 
-# 英语、法语、西班牙语、葡萄牙语、德语、意大利语        西欧Windows
-# 俄语                                                                                       西里尔文Windows
-# 阿拉伯语                                                                                 ASMO708
-# 中文                                                                                        简体中文（GB2312）    
-# 繁体中文                                                                                 繁体中文（Big5）    
-# 泰文                                                                                        泰文Windows
-# 越南语                                                                                    越南文Windows
-# 印尼语                                                                                    西欧Windows
-# 土耳其语                                                                                土耳其文Windows
-# 波斯语                                                                                    阿拉伯文Windows
-
-LANGUAGE_S='gb2312'
-LANGUAGE_E='cp1252'
-LANGUAGE_T='big5'
-LANGUAGE_P='cp1252'
-LANGUAGE_a='cp1252'
-LANGUAGE_I='cp1252'
-LANGUAGE_R='cp1251'
-LANGUAGE_F='cp1252'
-LANGUAGE_B='iso8859_6'
-LANGUAGE_G='cp1252'
-LANGUAGE_t='cp1254'
-
-
-CodeingMap={
-            'S':LANGUAGE_S,
-            'E':LANGUAGE_E,
-            'T':LANGUAGE_T,
-            'P':LANGUAGE_P,
-            'a':LANGUAGE_a,
-            'I':LANGUAGE_I,
-            'R':LANGUAGE_R,
-            'F':LANGUAGE_F,
-            'B':LANGUAGE_B,
-            'G':LANGUAGE_G,
-            't':LANGUAGE_t,
-            }
-
-# dest_dir=r'D:\work folder\tftp_share_point\objs'
-sub_dir='lang\\'
-
-def checkOneLine(aline,key):
-    head = key+ '/_'
+sub_dir = 'lang\\'
+#检查一列数据是否是有效数据
+def checkIsGoodCol(cloObj):
+    # 检查键值有效性
+    try:
+        key = cloObj[10][0];
+        if key not in Rules.CodeingMap:
+            return
+    except Exception:   # 无效列处理
+        print ('*** Discover a empty row !! ****')
+        return
+ 
+ 
+    
+def checkOneLine(aline, key):
+    head = key + '/_'
     return aline.startswith(head)
     
-    
 endline = os.linesep
-def buildOne(cloObj,dest_dir='.\language'):
-        key='0'
-        fname=''
-        
-        #检查键值有效性
-        try:
-            key = cloObj[10][0]
-            if key not in CodeingMap:
-                return
-        except Exception: #无效列处理
-            print ('*** Discover a empty row !! ****')
+def buildOne(cloObj, dest_dir='.\language'):
+        key = '0'
+        fname = ''
+
+        if not checkIsGoodCol(cloObj):
             return
         
-        #组装文件名
+        # 组装文件名
         if key in string.ascii_lowercase:
-            fname=sub_dir+'LANGEUAGE.'+ key
+            fname = sub_dir + 'LANGEUAGE.' + key
         else:
-            fname='LANGEUAGE.'+ key    
+            fname = 'LANGEUAGE.' + key    
         
-        #获取文件编码格式
-        ecodeType=CodeingMap[key]
-        print('DestDir:%10s '%(dest_dir))
-        print('DestFileName:%10s  encoding as -- %s -- '%(fname,ecodeType))
+        # 获取文件编码格式
+        ecodeType = Rules.CodeingMap[key]
+        print('DestDir:%10s ' % (dest_dir))
+        print('DestFileName:%10s  encoding as -- %s -- ' % (fname, ecodeType))
         
-        fboj = open(os.path.join(dest_dir,fname),'bw') 
+        fboj = open(os.path.join(dest_dir, fname), 'bw') 
         rowNun = 0
         for aline in cloObj:
-            rowNun = rowNun+1
+            rowNun = rowNun + 1
             try:
-                if checkOneLine(aline.strip(string.whitespace),key) == True: #检查文件数据有效性
+                if checkOneLine(aline.strip(string.whitespace), key):   # 检查文件数据有效性
 #                     print(aline)
                     aline = aline + endline
-                    coding_bytes=aline.encode(ecodeType)    #把 utf-8 格式转化成指定编码
+                    coding_bytes = aline.encode(ecodeType)   # 把 utf-8 格式转化成指定编码
                 else:
-                    print('*** invalid line %3d : [%s]  key=[%s] ***'%(rowNun,aline,key))
+                    print('*** invalid line %3d : [%s]  key=[%s] ***' % (rowNun, aline, key))
                     continue
             except Exception:
                 try:
-                    print('*** coding %s  row: %3d  %s Error... ***'%(ecodeType,rowNun,aline[0:aline.find('=')]))    
+                    print('*** coding %s  row: %3d  %s Error... ***' % (ecodeType, rowNun, aline[0:aline.find('=')]))    
                 except Exception:
-                    print('*** coding %s  row: %3d  [cant not print string] Error... ***'%(ecodeType,rowNun))
+                    print('*** coding %s  row: %3d  [cant not print string] Error... ***' % (ecodeType, rowNun))
             else:
                 fboj.write(coding_bytes)
         fboj.close()
-        print('\n\n')    
+        print('===')    
 
-def buildAll(excelfile='sourceLanguage.xls',dest_dir='.\language',sheetIndex=0):
-    for aclo in excelPack.getAllColsBySheetIndex(sheetIndex,excelfile):
-        buildOne(aclo,dest_dir)
+def buildAll(excelfile='sourceLanguage.xls', dest_dir='.\language', sheetIndex=0):
+    for aclo in excelPack.getAllColsBySheetIndex(sheetIndex, excelfile):
+        buildOne(aclo, dest_dir)
 
 def buildAllSheet(xlss):
-    dest_dir=''
-    for f in xlss:
-        dest_dir='obj\\'+ f[0:f.find('.xls')]
-        if os.path.exists(dest_dir) == False:
-            os.mkdir(dest_dir)
-            os.mkdir(os.path.join(dest_dir,sub_dir))
+    dest_dir = ''
+    for fileName in xlss:
+        #生成目标文件夹的名称为 
+        dest_dir = 'obj\\' + fileName[0:fileName.find('.xls')]
+        if not os.path.exists(os.path.join(dest_dir, sub_dir)):
+            os.makedirs(os.path.join(dest_dir, sub_dir))
         print (dest_dir)
-        buildAll(f,dest_dir)
+        buildAll(fileName, dest_dir)
+#         input('Build '+ fileName +' success! press any key to continue....')    
 
-
-if __name__ == '__main__':
+def main():
+    xlss = [xls for xls in os.listdir(path='.') if xls.endswith('.xls')]
+    buildAllSheet(xlss)
+    print('done...')
     
-    try:
-        xlss=[xls for xls in os.listdir(path='.') if xls.endswith('.xls')]
-        buildAllSheet(xlss)
-        input('Build %d success! press any key to continue....',xlss)
-    except  Exception as e:
-        print('buildAllSheet() Error: %s '%(e))
-        input('Error press any key to continue....')
-    finally:
-        pass
-
+    
+if __name__ == '__main__':
+    main()
+  
+  
+    
+#     try:
+#         xlss = [xls for xls in os.listdir(path='.') if xls.endswith('.xls')]
+#         buildAllSheet(xlss)
+#     except  Exception as e:
+#         print('buildAllSheet() Error: %s ' % (e))
+#     finally:
+#         input('done press any key to continue....')
+#         pass
